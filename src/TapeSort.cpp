929548e4d43a98b33ca495234eec2942d6457cc4
kitsunin-dev <stepanpilipcenko@gmail.com>
2024-05-21 22:58:25 +0300

    Tape sort improved

--- src/TapeSort.cpp
+++ src/TapeSort.cpp
@@ -65,28 +65,58 @@ void TapeSort::sort(TapeInterface& input_tape,
     input_tape.rewind();
     output_tape.rewind();
 
-    for (const std::string& temp_filename : temp_files) 
+    // вектора для слияния временных лент в одну сортированную
+    std::vector<std::ifstream> temp_streams(temp_files.size());
+    std::vector<int> current_values(temp_files.size(), INT_MAX);
+    std::vector<bool> has_next(temp_files.size(), true);
+
+    // открываем все временные файлы и считываем первые значения
+    for (size_t i = 0; i < temp_files.size(); ++i)
     {
-        // читаем временной файл
-        std::ifstream temp_file(temp_filename);
-        int value;
-        while (temp_file >> value) 
-        {
-            try 
+        temp_streams[i].open(temp_files[i]);
+        if (!(temp_streams[i] >> current_values[i]))
+            has_next[i] = false;
+    }
+
+    while (true) 
+    {
+        int min_value = INT_MAX;
+        size_t min_index = 0;
+        bool found = false;
+
+        // ищем минимальный элемент среди прочитанных
+        for (size_t i = 0; i < current_values.size(); ++i)
+            if (has_next[i] && current_values[i] < min_value) 
             {
-                // заносим элемент в выходную ленту
-                output_tape.write(value);
-                output_tape.forward();
-            } 
-            catch (const std::out_of_range& e) 
-            {
-                temp_file.close();
-                throw;
+                min_value = current_values[i];
+                min_index = i;
+                found = true;
             }
+
+        // если не нашли (если дошли до конца всех файлов), выходим из цикла
+        if (!found)
+            break;
+
+        // записываем этот элемент на выходную ленту
+        try 
+        {
+            output_tape.write(min_value);
+            output_tape.forward();
+        } 
+        catch (const std::out_of_range& e) 
+        {
+            throw e;
         }
 
-        temp_file.close();
+        // читаем следующий элемент из соотв. файла
+        if (!(temp_streams[min_index] >> current_values[min_index]))
+            has_next[min_index] = false;
     }
+
+    // закрываем временные файловые потоки
+    for (auto& stream : temp_streams)
+        if (stream.is_open())
+            stream.close();
 
     // удаляем директорию с временными файлами
     std::filesystem::remove_all(tmp_dir);
